2a44b884e2d5370ec69ce0401cbf2e800e8fbb37
Rida Naz <rnaz3414@gmail.com>
2025-12-07 21:32:40 +0500

    Update Github Url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/your-org/humanoid-robotics-book/tree/main/', // Placeholder for GitHub repo URL
+            'https://github.com/RidaNaz/Physical-AI-Humanoid-Robotics-TextBook',
         },
         // Blog is not required for this book, so it is removed.
         theme: {
